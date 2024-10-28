5b38264a629a5d9dd311e93e06dbf2d0609f2c11
waikoo <beroer@protonmail.com>
2024-10-28 21:37:42 +0100

    Update: move .git folder 1 lvl up

--- src/App.svelte
+++ src/App.svelte
@@ -12,19 +12,6 @@
     const value = target.dataset[name as UrlValue] as UrlValue;
 
     url = setUrl(url, name, value);
-    // if (name) {
-    //   if (url.includes(name)) {
-    //     const regex = new RegExp(
-    //       `${name}=(\\d+|easy|medium|hard|boolean|multiple)`,
-    //     );
-    //     url = url.replace(regex, `${name}=${value}`);
-    //   } else {
-    //     url =
-    //       url.at(-1) === "?"
-    //         ? `${url}${name}=${value}`
-    //         : `${url}&${name}=${value}`;
-    //   }
-    // }
   };
 </script>
 
