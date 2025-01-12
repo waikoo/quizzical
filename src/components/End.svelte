a3fb2c5332bfb44370b8fb7856ff9591f5863193
waikoo <beroer@protonmail.com>
2025-01-12 19:27:25 +0100

    Style: fix buttons on all viewports on end page

--- src/components/End.svelte
+++ src/components/End.svelte
@@ -43,21 +43,23 @@
 
 <div class="flex justify-center items-center min-h-[100vh]">
   <article
-    class="text-center p-[1px] rounded-[25px] mx-auto col-start-2 col-end-3 w-[301px] md:w-[494px]"
+    class="text-center p-[1px] rounded-[25px] mx-auto col-start-2 col-end-3 w-[301px] md:w-[494px] bg-gradient-to-b from-[#373636] to-[black] shadow-[0_0_2px_1px_black] xl:w-[452px]"
   >
     <div
-      class="flex flex-col gap-8 md:gap-20 bg-[#180F05] py-12 px-3 rounded-[25px]"
+      class="flex flex-col gap-8 md:gap-[68px] xl:gap-[3.5rem] bg-[#180F05] py-12 px-3 rounded-[25px] shadow-[0_15px_50px_#180f05] xl:w-[452px]"
     >
       <div>
         <FeedbackMessage gamePoints={$gamePoints} {questionLength} />
         <p
-          class="text-[1.063rem] text-[#DBCD9E] uppercase md:text-[1.438rem] xl:text-[1.25rem]"
+          class="font-['Anton'] text-[1.063rem] text-[#DBCD9E] uppercase md:text-[1.438rem] xl:text-[1.25rem]"
         >
           {$gamePoints}/{questionLength} answers correct
         </p>
       </div>
 
-      <div class="flex flex-col gap-8 md:gap-14 relative z-2">
+      <div
+        class="flex flex-col gap-8 md:gap-[28px] xl:gap-8 relative z-2 w-full md:w-[318px] xl:w-[265px] mx-auto"
+      >
         <ButtonTryAgain handler={tryAgain}>Try Again</ButtonTryAgain>
         <ButtonPlay handler={playDefault}>Play (Default)</ButtonPlay>
         <ButtonSettings handler={goToSettings} />
@@ -65,18 +67,3 @@
     </div>
   </article>
 </div>
-
-<style>
-  p {
-    font-family: "Anton", sans-serif;
-  }
-
-  article {
-    background: linear-gradient(#373636, black);
-    box-shadow: 0px 0px 2px 1px black;
-  }
-
-  article > div {
-    box-shadow: 0px 15px 50px #180f05;
-  }
-</style>
