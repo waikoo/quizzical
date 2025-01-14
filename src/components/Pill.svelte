e7a40cabe8bc21cc06c6a4e8a1c90c8b6911abb6
waikoo <beroer@protonmail.com>
2025-01-14 05:39:59 +0100

    Style: increase padding & extract variable of Pill

--- src/components/Pill.svelte
+++ src/components/Pill.svelte
@@ -7,13 +7,15 @@
     extraClass,
   }: { children: Snippet; textColorClass: string; extraClass: string } =
     $props();
+
+  const borderRadius = "rounded-[40px]";
 </script>
 
 <div
-  class={`${extraClass} py-[1px] px-[3px] bg-gradient-to-b from-[#2b2b2b] to-black shadow-[4px_4px_20px_black] rounded-[40px] mt-2 uppercase`}
+  class={`${extraClass} py-[1px] px-[4px] bg-gradient-to-b from-[#2b2b2b] to-black shadow-[4px_4px_20px_black] ${borderRadius} mt-2 uppercase`}
 >
   <p
-    class={`${textColorClass} text-center bg-[#180f05] rounded-[40px] p-2 block relative md:text-[1.25rem] font-['Anton']`}
+    class={`${textColorClass} text-center bg-[#180f05] py-[10px] px-[21px] ${borderRadius} block relative md:text-[1.25rem] font-['Anton']`}
   >
     {@render children()}
   </p>
